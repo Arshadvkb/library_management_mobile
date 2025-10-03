e5aa83a3325af437914bd8397a3fe82b3ccc938f
arshadvkb <arshadvkb118@gmail.com>
2025-10-03 15:43:55 +0530

    added checking condition in login

--- lib/app/login/login_controller.dart
+++ lib/app/login/login_controller.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
-import 'package:library_management_mobile/app/home/home_view.dart';
+
 
 
 class LoginController extends ChangeNotifier {
